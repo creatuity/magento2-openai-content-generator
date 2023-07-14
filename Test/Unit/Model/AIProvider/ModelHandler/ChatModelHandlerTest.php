1aa63f94ce2d85fc2ccd5fc240956f799f4e87c8
mpedzik <mpedzik@creatuity.com>
2023-07-14 12:58:53 +0200

    [INTACR-7] improvements

---
+++ Test/Unit/Model/AIProvider/ModelHandler/ChatModelHandlerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Creatuity\AIContentOpenAI\Test\Unit\Model\AIProvider\ModelHandler;
+
+use PHPUnit\Framework\TestCase;
+
+class ChatModelHandlerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+
+    }
+}
